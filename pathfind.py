d7658a27288825e2621f10955de54b65b152b3d3
janheuer <jan.heuer@me.com>
2018-11-01 12:02:23 +0100

    Neue Version

--- pathfind.py
+++ pathfind.py
@@ -1,119 +1,184 @@
-#script (python)
+# Programm aufgeteielt in pathfind.py + robot.py
+# -> nicht mehr über clingo aufrufen sondern nur mit python
+# Fuer anzeigen im visualizer: simulatorCostum.py (-> braucht die Dateien simulator.py und network.py aus visualizer/scripts)
+# Im visualizer starten über: Network->Initialize simulator, dort kommando zum starten des programms eingeben (also python ./pathfind.py) + port 5001 
+# Alternativ: Ausgaben durch print mit printer.py (-> dafür zeile 29 auskommentieren und dafür zeile 30 nutzen)
 
 import clingo
+import simulatorCostum
+import printer
+import robot
 
+class Pathfind(object):
+    def __init__(self, instance, encoding):
+        # speichern der instanz in entsprechenden datenstrukturen + erstellen der roboter
+        self.instance = instance
+        self.encoding = encoding
+        self.prg = clingo.Control()
+        self.prg.load(instance)
+        self.prg.ground([("base", [])])
+        self.state = [] # zum speichern auf welchen positionen roboter stehen
+        self.parse_instance()
+        # nodes: [[id, x, y]]
+        # highways: [[id, x, y]]
+        # robots: [robot]
+        # orders: [[id, product, pickingStation]]
+        # pickingstations: [[id, x, y]]
+        # shelves: [[id, x, y]]
+        # products: [[id, shelf]]
 
-def output(actlist):
-	for act in actlist:
-		print("occurs(object(robot,1),action(move,("+str(act[1])+","+str(act[0])+")),"+str(act[2])+").")
-	return
-	
+        # initialisieren des simulators
+        self.sim = simulatorCostum.SimulatorCostum()
+        #self.sim = printer.Printer() # keine verwendung des visualizers, statt dessen print ausgaben
+        self.sim.add_inits(self.get_inits())
 
+        # initialisieren der roboter
+        for robot in self.robots:
+            self.state[robot.pos[0]-1][robot.pos[1]-1] = 0
+            robot.update_state(self.state)
+            self.assign_order(robot)
+            robot.solve()
 
-def main(prg):
-	prg.ground([("base", [])])
+        self.t = 0
 
-	
+    def run(self):
+        while self.orders != []:#or zur zeit bearbeitete orders != []
+            self.t += 1
+            # blockierte felder zum testen
+            if (5>=self.t>=2):
+                self.state[4-1][6-1] = 0
+            if (16>=self.t>=13):
+                self.state[4-1][2-1] = 0
+            if (39>=self.t>=22):
+                self.state[8-1][2-1] = 0
+            for robot in self.robots:
+                robot.update_state(self.state)
+                if robot.action_possible():
+                    self.perform_action(robot)
+                else:
+                    # todo: wenn roboter auf pickingstation bewegen will, neu solven nicht sinnvoll
+                    # statdessen warten bis pickingstation wieder frei
+                    robot.solve()
+                    self.perform_action(robot)
+        # visualization starten
+        self.sim.run(self.t)
 
-	# festlegen der instanz
-	size = [5,5]
-	goal = [1,1]
-	start = [5,1]
-	# warteschlange von states: jeder besteht aus zeitpunkt des eintritts und matrix mit blockierten feldern
-	states = [(0, [	[1,1,1,1,1],
-					[1,0,1,1,1],
-					[0,1,1,0,1],
-					[1,1,0,1,1],
-					[1,1,0,1,1]]),
-	 	  (4, [		[1,1,1,1,1],
-					[0,1,1,1,1],
-					[0,1,0,0,1],
-					[1,1,0,1,1],
-					[1,1,0,1,1]]),
-		  (5, [		[1,0,1,1,1],
-					[1,1,1,1,1],
-					[0,1,0,0,1],
-					[1,1,0,1,1],
-					[1,1,0,1,1]])]
+    def parse_instance(self):
+        self.nodes = []
+        self.highways = []
+        self.robots = []
+        self.orders = []
+        order_stations = {} # zum zuordnen der orders zu pickingstation benoetigt
+        self.pickingstations = []
+        self.shelves = []
+        self.products = []
 
-	# externals festlegen
-	prg.assign_external(clingo.Function("start", start), True)
-	prg.assign_external(clingo.Function("goal", goal), True)
+        with self.prg.solve(yield_=True) as h:
+            # optimales modell auswaehlen
+            for m in h:
+                opt = m
+            for atom in opt.symbols(shown=True):
+                if atom.name == "init":
+                    name = atom.arguments[0].arguments[0].name
+                    if name == "node":
+                        id = atom.arguments[0].arguments[1].number
+                        x = atom.arguments[1].arguments[1].arguments[0].number
+                        y = atom.arguments[1].arguments[1].arguments[1].number
+                        self.nodes.append([id, x, y])
+                    elif name == "highway":
+                        id = atom.arguments[0].arguments[1].number
+                        x = atom.arguments[1].arguments[1].arguments[0].number
+                        y = atom.arguments[1].arguments[1].arguments[1].number
+                        self.highways.append([id, x, y])
+                    elif name == "robot":
+                        id = atom.arguments[0].arguments[1].number
+                        x = atom.arguments[1].arguments[1].arguments[0].number
+                        y = atom.arguments[1].arguments[1].arguments[1].number
+                        self.robots.append(robot.Robot(id, [x,y], self.encoding, self.instance))
+                    elif name == "order":
+                        id = atom.arguments[0].arguments[1].number
+                        if atom.arguments[1].arguments[0].name == "line":
+                            product = atom.arguments[1].arguments[1].arguments[0].number
+                            self.orders.append([id,product])
+                        else:
+                            station = atom.arguments[1].arguments[1].number
+                            order_stations[id] = station
+                    elif name == "pickingStation":
+                        id = atom.arguments[0].arguments[1].number
+                        x = atom.arguments[1].arguments[1].arguments[0].number
+                        y = atom.arguments[1].arguments[1].arguments[1].number
+                        self.pickingstations.append([id,x,y])
+                    elif name == "product":
+                        id = atom.arguments[0].arguments[1].number
+                        shelf = atom.arguments[1].arguments[1].arguments[0].number
+                        # amount
+                        self.products.append([id,shelf])
+                    elif name == "shelf":
+                        id = atom.arguments[0].arguments[1].number
+                        x = atom.arguments[1].arguments[1].arguments[0].number
+                        y = atom.arguments[1].arguments[1].arguments[1].number
+                        self.shelves.append([id,x,y])
 
-	# initialisierung für schleife
-	current_state = states[0][1] # speichert im moment blockierte felder
-	states = states[1:] # aktuellen state aus warteschlange entfernen
-	current_pos = start
-	goal_reached = False
-	solved = False # zum bestimmen wann erneut solve aufgerufen wird
-	t = 1 # überprüfen der schritte beginnt ab t=1 (da bei t=0 keine bewegung)
-	t_offset = 0 # zeit offset zum speichern der gesamt vergangenen zeit, da t für jedes solve zurückgesetzt wird
+        # pickingstations zu orders zuteilen
+        for order in self.orders:
+            order.append(order_stations[order[0]])
 
-	# für print ausgaben in der schleife am besten mit clingo option --outf=3 ausführen
-	# ausgaben zeigen gemachte bewegung an und wann auf ein blockiertes feld gelaufen würde
-	
-	print("init(object(robot,1),value(at,("+str(start[1])+","+str(start[0])+"))).")
-	for i in range(size[0]):
-		for j in range(size[1]):
-			print("init(object(node,"+str(i+j*size[1]+1)+"),value(at,("+str(i+1)+","+str(j+1)+"))).")
-	print("init(object(pickingStation,1),value(at,("+str(goal[1])+","+str(goal[0])+"))).")
-	
-	actlist = []
-	
-	while(not goal_reached):
-		if states != [] and states[0][0] == t+t_offset:
-			# block atome müssen aktualisiert werden
-			current_state = states[0][1] # speicher des neuen states
-			states = states[1:] # entfernen des states aus der warteschlange
-		if (not solved):
-			solved = True
-			opt_model = []
-			# assign aller blocks
-			for i in range(len(current_state)):
-				for j in range(len(current_state[0])):
-					prg.assign_external(clingo.Function("node", [i+1,j+1]), current_state[i][j])
-			prg.assign_external(clingo.Function("start", start), False) # alter start wird falsch
-			prg.assign_external(clingo.Function("start", current_pos), True) # neuer start wird true
-			start = current_pos
-			with prg.solve(yield_=True) as h:
-				# auswählen des letzten (also des optimalen) modells
-				for m in h:
-					opt = m
-				# kopieren des modells
-				for atom in opt.symbols(shown=True):
-					opt_model.append(atom)
-			
-		for atom in opt_model:
-			if (atom.name == 'pos' and atom.arguments[2].number == t-1): # position am ende des vorherigen zeitschritts
-				current_pos = [atom.arguments[0].number,atom.arguments[1].number]
-				if current_pos == goal: # falls zeil erreicht ist kann schleife abgebrochen werden
-					goal_reached = True
-					#print("The goal has been reached")
-					output(actlist)
-					return
-			elif (atom.name == "move" and atom.arguments[2].number == t): # move welcher in diesem zeitschritt ausgeführt werden soll
-				move_dir = [atom.arguments[0].number,atom.arguments[1].number]
-				
-		next_x = current_pos[0]+move_dir[0]
-		next_y = current_pos[1]+move_dir[1]
-		if current_state[next_x-1][next_y-1] == 0:
-			# position auf die sich der roboter bewegen will ist blockiert
-			#print("pos("+str(current_pos[0])+","+str(current_pos[1])+","+str(t+t_offset-1)+")")
-			#print("move("+str(move_dir[0])+","+str(move_dir[1])+","+str(t+t_offset)+") would move robot onto block("+str(next_x)+","+str(next_y)+")")
-			solved = False # es muss erneut gesolved werden
-			# t wird auf 1 zurückgesetzt, speichern der vergangenen zeit im offset
-			t_offset += t-1
-			t = 1
-		else:
-			# move kann durchgeführt werden
-			#print("pos("+str(current_pos[0])+","+str(current_pos[1])+","+str(t+t_offset-1)+")")
-			#print("move("+str(move_dir[0])+","+str(move_dir[1])+","+str(t+t_offset)+")")
-			actlist.append([move_dir[0], move_dir[1], t+t_offset])
-			t += 1 # sonst nächsten zeitschritt prüfen
+        # self.state initialisieren
+        for i in range(max(self.nodes, key=lambda item:item[1])[1]):
+            self.state.append([])
+            for j in range(max(self.nodes, key=lambda item:item[2])[2]):
+                self.state[i].append(1)
 
-	
-#end.
+    def assign_order(self, robot):
+        # todo: keine ueberschneidungen in zugeteielten orders
+        robot.set_order(self.orders[0][0], self.orders[0][1], self.orders[0][2])
+        # aus orders loeschen und in liste mit zur zeit bearbeiteten orders speichern
 
-#external node(1..X, 1..Y) : size(X, Y).
-#external start(1..X, 1..Y) : size(X, Y).
-#external goal(1, 1).
+    def perform_action(self, robot):
+        prev_pos = robot.pos
+        name, args = robot.action()
+        self.sim.add(robot.id, name, args, self.t)
+        if name == "move":
+            self.state[prev_pos[0]-1][prev_pos[1]-1] = 1
+            self.state[robot.pos[0]-1][robot.pos[1]-1] = 0
+        elif name == "putdown":
+            self.update_orders(robot.order)
+            if self.orders != []:
+                self.assign_order(robot)
+                robot.solve()
+
+    def update_orders(self, order):
+        # aus liste mit zur zeit bearbeiteten order speichern
+        self.orders.remove(order)
+
+    def get_inits(self):
+        # alle inits werden als string zur liste inits hinzugefuegt, diese wird dem simulator uebergebn
+        inits = []
+        for node in self.nodes:
+            if node not in [[59,4,6], [15,4,2], [19,8,2]]: # blockierte felder zum testen
+                inits.append("init(object(node,"+str(node[0])+"),value(at,("+str(node[1])+","+str(node[2])+")))")
+        for highway in self.highways:
+            if highway not in [[59,4,6], [15,4,2], [19,8,2]]: # blockierte felder zum testen
+                inits.append("init(object(highway,"+str(highway[0])+"),value(at,("+str(highway[1])+","+str(highway[2])+")))")
+        for robot in self.robots:
+            inits.append("init(object(robot,"+str(robot.id)+"),value(at,("+str(robot.pos[0])+","+str(robot.pos[1])+")))")
+            if robot.pickupdone:
+                inits.append("init(object(robot,"+str(robot.id)+"),value(carries,"+str(robot.shelf)+")))")
+        for station in self.pickingstations:
+            inits.append("init(object(pickingStation,"+str(station[0])+"),value(at,("+str(station[1])+","+str(station[2])+")))")
+        for shelf in self.shelves:
+            inits.append("init(object(shelf,"+str(shelf[0])+"),value(at,("+str(shelf[1])+","+str(shelf[2])+")))")
+        order_stations = []
+        for order in self.orders:
+            inits.append("init(object(order,"+str(order[0])+"),value(line,("+str(order[1])+",1)))")
+            # zuordnung zu pickingstation nur einmal uebergeben
+            if order[0] not in order_stations:
+                order_stations.append(order[0])
+                inits.append("init(object(order,"+str(order[0])+"),value(pickingStation,"+str(order[2])+"))")
+        for product in self.products:
+            inits.append("init(object(product,"+str(product[0])+"),value(on,("+str(product[1])+",1)))")
+        return inits
+
+if __name__ == "__main__":
+    pathfind = Pathfind('./instance.lp', './pathfind.lp')
+    pathfind.run()
