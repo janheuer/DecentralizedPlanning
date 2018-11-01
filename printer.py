6a8d094c0d1052444cf4ac6d6043e05d8bf99424
janheuer <jan.heuer@me.com>
2018-11-01 12:04:38 +0100

    Klasse für alternative Ausgabe durch prints

---
+++ printer.py
@@ -0,0 +1,17 @@
+class Printer(object):
+    def add_inits(self, inits):
+        for atom in inits:
+            print(atom)
+
+    def add(self, rid, name, args, t):
+        txt = "occurs(object(robot,"+str(rid)+"),action("+name+",("
+        if name == "move":
+            txt += str(args[0])+","+str(args[1])
+        elif name == "deliver":
+            txt += str(args[0])+","+str(args[1])
+        txt += ")),"+str(t)+")"
+
+        print(txt)
+
+    def run(self, steps):
+        return
