1594e41d5ac999b9f0ab6dbfaf673edd18db9c17
janheuer <jan.heuer@me.com>
2018-12-11 12:42:43 +0100

    Fehlerbehebung in release_order

--- pathfind.py
+++ pathfind.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# Programm aufgeteielt in pathfind.py + robot.py
-# -> nicht mehr über clingo aufrufen sondern nur mit python
-# Fuer anzeigen im visualizer: simulatorCostum.py (-> braucht die Dateien simulator.py und network.py aus visualizer/scripts)
-# Im visualizer starten ueber: Network->Initialize simulator, dort kommando zum starten des programms eingeben (also python ./pathfind.py) + port 5001
-# Alternativ: Ausgaben durch print mit printer.py (-> dafuer zeile 29 auskommentieren und dafuer zeile 30 nutzen)
-
 import clingo
-import simulatorCostum
+import simulatorCostum # braucht die Dateien simulator.py und network.py aus visualizer/scripts
 import printer
 import robot
 from time import time
 import argparse
 
 class Pathfind(object):
-    def __init__(self, instance, encoding, output, benchmark, solve_output):
+    def __init__(self, instance, encoding, output, benchmark, solve_output, port):
         self.output = output
         self.benchmark = benchmark
         self.solve_output = solve_output
@@ -56,13 +50,12 @@ class Pathfind(object):
 
         # initialisieren des simulators
         if self.output == "viz":
-            self.sim = simulatorCostum.SimulatorCostum()
+            self.sim = simulatorCostum.SimulatorCostum(port)
         elif self.output == "print":
             self.sim = printer.Printer() # keine verwendung des visualizers, statt dessen print ausgaben
 
-        if not benchmark:
-            if self.output is not None:
-                self.sim.add_inits(self.get_inits())
+        if self.output is not None:
+            self.sim.add_inits(self.get_inits())
 
         # initialisieren der roboter
         for robot in self.robots:
@@ -170,62 +163,64 @@ class Pathfind(object):
             self.state[r.pos[0]-1][r.pos[1]-1] = 0
 
     def assign_order(self, robot):
+        # choose first order which can be completed
         possible_shelves = []
-
-        # auswählen der ersten ausführbaren order
         o = -1
         possible_order = False
-        while not possible_order:
+        # no possible order and not all orders checked
+        while (not possible_order) and (o != len(self.orders)-1):
             o += 1
-            # alle shleves die das produkt haben
+            # list of all shelves which have the needed product
             possible_shelves = [shelf for [id, shelf] in self.products if id == self.orders[o][1]]
-            # entfernen der shelves die bereits benutzt werden
+            # remove already used shelves
             for id in self.used_shelves:
                 if id in possible_shelves:
                     possible_shelves.remove(id)
-            # wenn es mögliche shleves gibt ist order ausführbar
+            # if there are shelves the order is possible
             if possible_shelves != []:
                 possible_order = True
-            # order ist nicht ausführbar -> gibt es noch nicht überprüfte orders?
-            elif o == len(self.orders)-1:
-                # falls keine weiteren orders vorhanden sind kann keine order zegeordnet werden
-                break
         if possible_order:
-            robot.set_order(self.orders[o][0], self.orders[o][1], self.orders[o][2], possible_shelves)
+            robot.set_order(self.orders[o], possible_shelves)
             self.reserve_order(self.orders[o])
         return possible_order
 
     def perform_action(self, robot):
+        # mark old position as free
         self.state[robot.pos[0]-1][robot.pos[1]-1] = 1
         name, args = robot.action()
         if name == "":
-            # roboter hat in diesem timestep nichts zu tun:
-            # 1) es sind keine orders mehr vorhanden
-            # oder
-            # 2) im letzen timestep konnte keine order zugeteielt werden
-            if self.orders != []:
-                self.plan(robot)
-        if (self.output is not None) and (name!=""):
-            if not benchmark:
-                self.sim.add(robot.id, name, args, self.t)
+            # robot doesn't have to do a action in this timestep because
+            # 1) there aren't any more orders
+            # 2) in the last timestep no order could be assigned
+            # 3) robot was in a deadlock in the last timestep
+            self.plan(robot)
+        if (self.output is not None) and (name != ""):
+            self.sim.add(robot.id, name, args, self.t)
         if name == "putdown":
+            # order is finished
             self.finish_order(robot.order, robot.shelf)
             robot.release_order()
-            if self.orders != []:
-                self.plan(robot)
+            # plan a new order
+            self.plan(robot)
+        # mark new position
         self.state[robot.pos[0]-1][robot.pos[1]-1] = 0
 
     def finish_order(self, order, shelf):
-        # order was completed
+        # shelf can be used again
         self.release_shelf(shelf)
+        # order was delivered
         self.orders_in_delivery.remove(order)
 
     def release_order(self, order):
+        # order not in delivery
         self.orders_in_delivery.remove(order)
-        self.orders.append(order)
+        # order wasn't completed
+        self.orders.append(list(order))
 
     def reserve_order(self, order):
+        # order now in delivery
         self.orders_in_delivery.append(order)
+        # remove from still open orders
         self.orders.remove(order)
 
     def reserve_shelf(self, shelf):
@@ -238,9 +233,9 @@ class Pathfind(object):
 
     def plan(self, robot):
         if robot.shelf == -1: # robot doesn't have a order assigned
+            resolve = False
             if not self.assign_order(robot): # try to assign a order
                 return False
-            resolve = False
             if self.solve_output:
                 print("robot"+str(robot.id)+" planning order id="+str(robot.order[0])+" product="+str(robot.order[1])+" station="+str(robot.order[2])+" at t="+str(self.t))
         else:
@@ -308,9 +303,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("instance", help="the instance to be loaded")
-    parser.add_argument("-b", "--benchmark", help="use benchmark output (set other output to None)", default=False, action="store_true")
-    parser.add_argument("-o", "--output", help="output mode (default=print)", choices=["print", "viz"], default="print", )
+    parser.add_argument("-b", "--benchmark", help="use benchmark output (turns off all other output)", default=False, action="store_true")
+    parser.add_argument("-o", "--output", help="output mode (default=print)", choices=["print", "viz"], default="print")
     parser.add_argument("-n", "--nosolve", help="turns off output when a robot solves/resolves", default=False, action="store_true")
+    parser.add_argument("-p", "--port", help="port to be used when connecting to the visualizer (default=5001)", default=5001, type=int)
     args = parser.parse_args()
 
     benchmark = args.benchmark
@@ -319,7 +315,7 @@ if __name__ == "__main__":
 
     if benchmark:
         t1 = time()
-    pathfind = Pathfind(args.instance, './pathfind.lp', output, benchmark, solve_output)
+    pathfind = Pathfind(args.instance, './pathfind.lp', output, benchmark, solve_output, args.port)
     if benchmark:
         t2 = time()
         initTime = t2-t1
