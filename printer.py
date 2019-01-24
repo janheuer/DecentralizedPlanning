697d9e0da82d13feca50c3d6d142067070652e4a
janheuer <jan.heuer@me.com>
2019-01-24 18:38:09 +0100

    Anpassung für wait atom
    -wait atom wird nicht mehr ausgegeben da nicht in asprilo-Spezifikation
    -run function nicht mehr gebraucht

--- printer.py
+++ printer.py
@@ -4,14 +4,13 @@ class Printer(object):
             print(atom + ".")
 
     def add(self, rid, name, args, t):
-        txt = "occurs(object(robot,"+str(rid)+"),action("+name+",("
-        if name == "move":
-            txt += str(args[0])+","+str(args[1])
-        elif name == "deliver":
-            txt += str(args[0])+","+str(args[1])
-        txt += ")),"+str(t)+")."
+        # for wait no atom is printed
+        if name != "wait":
+            txt = "occurs(object(robot,"+str(rid)+"),action("+name+",("
+            if name == "move":
+                txt += str(args[0])+","+str(args[1])
+            elif name == "deliver":
+                txt += str(args[0])+","+str(args[1])
+            txt += ")),"+str(t)+")."
 
-        print(txt)
-
-    def run(self, steps):
-        return
+            print(txt)
