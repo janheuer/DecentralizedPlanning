c12ff0aa70bfe90f3d7c4d29da19d48e5b354b1b
Jan Heuer <jan.heuer@me.com>
2020-10-01 16:44:15 +0200

    Fixed error in createInstances script + updated instances

--- instances/createInstances.sh
+++ instances/createInstances.sh
@@ -2,10 +2,10 @@
 for file in ./original/*.lp ./original/*/*.lp
 do
     newfile="./graph/${file#"./original/"}"
-    clingo --out-atomf='%s.' original/instance.lp createGraph.lp | head -n 5 | tail -n 1 > $newfile
+    clingo --out-atomf='%s.' $file createGraph.lp | head -n 5 | tail -n 1 > $newfile
     sed -i 's/ /&\n/g' $newfile
 
     newfile="./traffic/${file#"./original/"}"
-    clingo --out-atomf='%s.' original/instance.lp createTraffic.lp | head -n 5 | tail -n 1 > $newfile
+    clingo --out-atomf='%s.' $file createTraffic.lp | head -n 5 | tail -n 1 > $newfile
     sed -i 's/ /&\n/g' $newfile
 done
